bc0e1fea95f363b91893b316897d90a24814cd41
Prateek <codes4prateek@gmail.com>
2024-07-08 00:56:20 +0530

    Feat : Welcome Screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Chat from './components/chat.js';
 import { Analytics } from "@vercel/analytics/react"
+import Loader from './components/loader.js';
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <div className="App">
-      <Chat />
-      <Analytics/>
+      {loading ? <Loader /> : <Chat />}
+      <Analytics />
     </div>
   );
 }
---
+++ src/components/loader.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const Loader = () => {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-stone-900">
+      {/* Animation for falling text */}
+      <motion.div
+        initial={{ y: -100 }}
+        animate={{ y: 0 }}
+        transition={{ type: 'spring', stiffness: 80 }}
+        className="text-white text-4xl font-bold relative"
+      >
+        Welcome to ChatMate
+      </motion.div>
+
+      {/* Animation for balls */}
+      <motion.div
+        initial={{ x: -100, y: 50 }}
+        animate={{ x: 100, y: 50 }} 
+        transition={{ duration: 1.5, repeat: Infinity, repeatType: "reverse", ease: "linear" }}
+        className="absolute top-1/2 transform -translate-y-1/2 h-10 w-10 rounded-full bg-gray-500"
+      />
+      <motion.div
+        initial={{ x: 100, y: 50 }} 
+        animate={{ x: -100, y: 50 }} 
+        transition={{ duration: 1.5, repeat: Infinity, repeatType: "reverse", ease: "linear" }}
+        className="absolute top-1/2 transform -translate-y-1/2 h-10 w-10 rounded-full bg-gray-500"
+      />
+    </div>
+  );
+};
+
+export default Loader;
