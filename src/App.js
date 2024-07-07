f329ec8bf61e7e6bc0e7bd1f36302b87580df936
Prateek <codes4prateek@gmail.com>
2024-07-08 00:59:54 +0530

    Feat: Speed Insights

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import Chat from './components/chat.js';
 import { Analytics } from "@vercel/analytics/react"
 import Loader from './components/loader.js';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 function App() {
   const [loading, setLoading] = useState(true);
   useEffect(() => {
@@ -16,6 +17,7 @@ function App() {
     <div className="App">
       {loading ? <Loader /> : <Chat />}
       <Analytics />
+      <SpeedInsights/>
     </div>
   );
 }
